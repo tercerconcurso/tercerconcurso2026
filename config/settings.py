539a72b577e36b37737eb559e3dfb6b44fe4dcb5
Samanta Tropa <samanatatropa@gmail.com>
2026-04-05 12:24:24 -0400

    cambia base de datos a SQLite para producción

--- config/settings.py
+++ config/settings.py
@@ -83,15 +83,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'fertilidad_dev',
-        'USER': 'postgres',
-        'PASSWORD': 'fertilidad123',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
