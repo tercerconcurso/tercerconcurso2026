72c84e780dfacd21b8a3492c07b1e86655b07eff
Samanta Tropa <samanatatropa@gmail.com>
2026-04-08 17:35:03 -0400

    quitar superusuario de settings

--- config/settings.py
+++ config/settings.py
@@ -137,13 +137,3 @@ import os
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
-
-if not User.objects.filter(username="admin").exists():
-    User.objects.create_superuser(
-        username="admin",
-        email="admin@mail.com",
-        password="admin123"
-    )
