4b47666b046f20e658e6722c48d8dcbaccc63774
Samanta Tropa <samanatatropa@gmail.com>
2026-04-09 10:26:15 -0400

    fix real: handle dj_database_url import

--- config/settings.py
+++ config/settings.py
@@ -81,7 +81,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-import dj_database_url
+try:
+    import dj_database_url
+except ImportError:
+    dj_database_url = None
 
 DATABASES = {
     'default': dj_database_url.parse('postgresql://tercerconcurso_db_user:kddABlF9Nlsk0wn5K0U9ilUREKmK1dk7@dpg-d79ad0shg0os73e48n3g-a.oregon-postgres.render.com/tercerconcurso_db')
