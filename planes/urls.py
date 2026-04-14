326071160a4d4472340907ecdd054b7b6e492ed3
Samanta Tropa <samanatatropa@gmail.com>
2026-04-14 02:33:40 -0400

    fix views import comprobante

--- planes/urls.py
+++ planes/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import ver_constancia_pdf
 from .views import agenda_view
+from . import views
 
 urlpatterns = [
     path('constancia/<int:plan_id>/', ver_constancia_pdf, name='ver_constancia_pdf'),
