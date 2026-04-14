eb2a3d5405c16e03af1d057e1450785c2a01eaf6
Samanta Tropa <samanatatropa@gmail.com>
2026-04-14 00:57:54 -0400

    fix: email con variables de entorno

--- config/settings.py
+++ config/settings.py
@@ -155,7 +155,9 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = 'fertilidad.losrios@gmail.com'
-EMAIL_HOST_PASSWORD = 'neyu szvo swvp yvev'
+import os
+
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
